e0021377f177ccd6c2a6599c7aab17e1b1b9c418
Anton Kupayhorodskyy <a.kupayhorodskyy@gmail.com>
2020-09-14 21:37:37 -0500

    reformatted the code

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # snake-game
-A Python snake game project
+A Python snake game project made with Pygame.
 Use the arrow keys to play
--- main.py
+++ main.py
@@ -3,34 +3,47 @@ import pygame
 import math
 
 pygame.init()
+clock = pygame.time.Clock()
 pygame.display.set_caption("Snake")
 
-clock = pygame.time.Clock()
-
-screen = pygame.display.set_mode((800, 600))
-
-snake = [
-    [60, 300],
-    [40, 300],
-    [20, 300]
-]
+screen_width = 800
+screen_height = 600
+block_size = 20
 
 apple_color = (214, 21, 21)
-apple_x = random.randint(0, 780)
-apple_y = random.randint(0, 580)
-
 snake_color = (0, 117, 14)
-xChange = 20
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+snake = [
+    [block_size * 3, screen_height / 2],
+    [block_size * 2, screen_height / 2],
+    [block_size, screen_height / 2]
+]
+
+apple_x = 0
+apple_y = 0
+
+
+def reset_apple():
+    global apple_x, apple_y
+    apple_x = random.randint(0, screen_width - block_size)
+    apple_y = random.randint(0, screen_height - block_size)
+
+
+reset_apple()
+
+xChange = block_size
 yChange = 0
 
 
 def draw_snake():
     for block in snake:
-        pygame.draw.rect(screen, snake_color, (block[0], block[1], 20, 20))
+        pygame.draw.rect(screen, snake_color, (block[0], block[1], block_size, block_size))
 
 
 def draw_apple():
-    pygame.draw.rect(screen, apple_color, (apple_x, apple_y, 20, 20))
+    pygame.draw.rect(screen, apple_color, (apple_x, apple_y, block_size, block_size))
 
 
 # Go through each element of snake starting from the end and copy the element preceding the current one
@@ -44,7 +57,7 @@ def move_snake():
 # Check for collisions with walls or self
 def check_collisions():
     i = 1
-    if snake[0][0] >= 800 or snake[0][0] <= 0 or snake[0][1] <= 0 or snake[0][1] >= 600:
+    if snake[0][0] >= screen_width or snake[0][0] <= 0 or snake[0][1] <= 0 or snake[0][1] >= screen_height:
         return True
     while i < len(snake):
         if snake[0][0] == snake[i][0] and snake[0][1] == snake[i][1]:
@@ -55,7 +68,7 @@ def check_collisions():
 
 # Check for collisions with apple
 def check_apple():
-    if math.sqrt(math.pow((snake[0][0] - apple_x), 2) + math.pow((snake[0][1] - apple_y), 2)) <= 20:
+    if math.sqrt(math.pow((snake[0][0] - apple_x), 2) + math.pow((snake[0][1] - apple_y), 2)) <= block_size:
         return True
     return False
 
@@ -63,8 +76,7 @@ def check_apple():
 def grow_snake():
     global apple_x, apple_y
     snake.append([apple_x, apple_y])
-    apple_x = random.randint(0, 780)
-    apple_y = random.randint(0, 580)
+    reset_apple()
     move_snake()
 
 
@@ -74,7 +86,7 @@ running = True
 # variable to prevent two keys being pressed at the same time
 key_pressed = False
 while running:
-    clock.tick(20)
+    clock.tick(block_size)
     screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
@@ -83,19 +95,19 @@ while running:
                 if event.key == pygame.K_UP:
                     if yChange == 0:
                         xChange = 0
-                        yChange = -20
+                        yChange = -block_size
                 elif event.key == pygame.K_DOWN:
                     if yChange == 0:
                         xChange = 0
-                        yChange = 20
+                        yChange = block_size
                 elif event.key == pygame.K_LEFT:
                     if xChange == 0:
                         yChange = 0
-                        xChange = -20
+                        xChange = -block_size
                 elif event.key == pygame.K_RIGHT:
                     if xChange == 0:
                         yChange = 0
-                        xChange = 20
+                        xChange = block_size
         elif event.type == pygame.QUIT:
             running = False
     key_pressed = False
@@ -111,5 +123,4 @@ while running:
         running = False
     draw_snake()
     draw_apple()
-
     pygame.display.update()
